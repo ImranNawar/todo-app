082ba70b45394481532f7af35c93c9e6d8dd54d4
ImranNawar <imran1nawar@gmail.com>
2023-04-16 15:02:29 +0500

    Bootstrap React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 // import App from './App';
 import App from './todo/App'
 import Todo from './components/Todo'
+import Header from './components/Header'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +13,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Todo /> */}
+    <Header />
     <App />
   </React.StrictMode>
 );
