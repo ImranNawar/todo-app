bcc4ea767a3eeae9272181c8a14ff614aca30c10
ImranNawar <imran1nawar@gmail.com>
2023-10-09 12:25:40 +0500

    Modify the Navbar

--- src/components/NavbarC.js
+++ src/components/NavbarC.js
@@ -2,25 +2,26 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const NavbarC = () => {
   return (
     <Navbar style={{ backgroundColor: '#9ec5e5' }} className="p-3 mb-4 ">
       <Container>
-        <Navbar.Brand href="#home" className='fs-5 fw-bold'>
+        <Navbar.Brand as={Link} to="/" className='fs-4 fw-bold'>
           Todo App
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
 
-        <Nav variant="pills" defaultActiveKey="/">
-          <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#about" >About</Nav.Link>
-          </Nav.Item>
-        </Nav>
+          <Nav variant="pills">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav.Item>
+          </Nav>
 
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
